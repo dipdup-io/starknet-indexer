1050b67db86ca7826b12c28cd701fddbd51716a4
Artem <aopoltorzhicky@gmail.com>
2023-07-12 20:42:50 +0100

    Fix: comments of ProxyUpgrade entity

--- internal/storage/proxy_upgrade.go
+++ internal/storage/proxy_upgrade.go
@@ -29,15 +29,15 @@ type ProxyUpgrade struct {
 	// nolint
 	tableName struct{} `pg:"proxy_upgrade"`
 
-	ID         uint64      `pg:",comment:Unique internal identity"`
-	ContractID uint64      `pg:",comment:Proxy contract id"`
-	Hash       []byte      `pg:",comment:Proxy contract hash"`
-	Selector   []byte      `pg:",comment:Proxy contract selector (for modules)"`
-	EntityType EntityType  `pg:",use_zero,comment:Entity type behind proxy (0 - class | 1 - contract)"`
-	EntityID   uint64      `pg:",comment:Entity id behind proxy"`
-	EntityHash []byte      `pg:",comment:Entity hash behind proxy"`
-	Height     uint64      `pg:",comment:Height when event occured"`
-	Action     ProxyAction `pg:",comment:Action which occured with proxy (0 - add | 1 - update | 2 - delete)"`
+	ID         uint64      `comment:":Unique internal identity"`
+	ContractID uint64      `comment:":Proxy contract id"`
+	Hash       []byte      `comment:":Proxy contract hash"`
+	Selector   []byte      `comment:":Proxy contract selector (for modules)"`
+	EntityType EntityType  `pg:",use_zero" comment:"Entity type behind proxy (0 - class | 1 - contract)"`
+	EntityID   uint64      `comment:":Entity id behind proxy"`
+	EntityHash []byte      `comment:":Entity hash behind proxy"`
+	Height     uint64      `comment:":Height when event occured"`
+	Action     ProxyAction `comment:":Action which occured with proxy (0 - add | 1 - update | 2 - delete)"`
 }
 
 // NewUpgradeFromProxy -
