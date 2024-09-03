ee33e09468fc2e141e14be12f103a964a51be788
Artem <aopoltorzhicky@gmail.com>
2024-09-03 11:40:05 +0300

    Support 0.13.2.1

--- pkg/indexer/parser/parser.go
+++ pkg/indexer/parser/parser.go
@@ -30,7 +30,7 @@ func createParser(
 		"0.10.0", "0.10.1", "0.10.2", "0.10.3",
 		"0.11.0", "0.11.0.2", "0.11.1", "0.11.2",
 		"0.12.0", "0.12.1", "0.12.2", "0.12.3",
-		"0.13.0", "0.13.1", "0.13.1.1", "0.13.2":
+		"0.13.0", "0.13.1", "0.13.1.1", "0.13.2", "0.13.2.1":
 		return v0.New(resolver, cache, blocks), nil
 	default:
 		return nil, errors.Errorf("unknown starknet version: %s", *version)
